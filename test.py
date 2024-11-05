10681f2520adbb0254fb99c8d49d09592ae710a7
acosidn <alessandro.cosci@sidn.es>
2024-11-05 09:44:30 +0100

    added lookup func for container_gtm_id

--- test.py
+++ test.py
@@ -5,7 +5,10 @@ from pprint import pprint as print
 from typing import List
 
 SERVICE_ACCOUNT_FILE = './service_account.json'
-SCOPES = ['https://www.googleapis.com/auth/tagmanager.manage.users']
+SCOPES = [
+  'https://www.googleapis.com/auth/tagmanager.manage.users',
+  'https://www.googleapis.com/auth/tagmanager.readonly'
+]
 
 def get_credentials(service_account_file:str, scopes:List[str]) -> Credentials:
   creds = Credentials.from_service_account_file(service_account_file, scopes=scopes)
@@ -124,6 +127,22 @@ def update_tag_manager_user_access(
   response = request.execute()
   return response
 
+def lookup_container_by_container_tag_id(container_tag_id:str) -> dict:
+  """Looks up a container by its container tag ID.
+
+  Args:
+      container_tag_id (str): The unique identifier of the container.
+
+  Returns:
+      A dictionary representing the container information, or None if not found.
+  """
+
+  creds = get_credentials(SERVICE_ACCOUNT_FILE, SCOPES)
+  service = build('tagmanager', 'v2', credentials=creds)
+
+  container = service.accounts().containers().lookup(tagId=container_tag_id).execute()
+  return container
+
 def list_tag_manager_accesses(account_id:str) -> dict:
   """Lists the accesses for a given Tag Manager account.
 
@@ -146,16 +165,16 @@ def list_tag_manager_accesses(account_id:str) -> dict:
 
 def add_or_update_permission(
   account_id:str,
-  container_id:str,
   user_email:str,
   account_permission:str,
-  container_permission:str
+  container_permission:str,
+  container_id:str | None  = None,
+  container_tag_id:str | None = None,
 ) -> dict:
   """Adds or updates the access of an existing user for a given Tag Manager account and container.
 
   Args:
     account_id (str): The ID of the Tag Manager account.
-    container_id (str): The ID of the Tag Manager container.
     user_email (str): The email address of the user to add.
     account_permission (str, optional): The permission level for the user on the entire account. Defaults to 'accountPermissionUnspecified'. Valid options are:
       - 'accountPermissionUnspecified': Unspecified permission level (not recommended).
@@ -169,10 +188,22 @@ def add_or_update_permission(
       - 'noAccess': Revokes any access to the container.
       - 'publish': Allows publishing container versions.
       - 'read': Allows viewing the container content.
+    container_id (str or None): The ten digits ID of the Tag Manager container.
+    container_tag_id (str or None): The Tag ID of the Tag Manager container, example: GTM-12345.
 
   Returns:
     dict: The newly created or updated user access object as a dictionary.
   """
+
+  if container_id == None and container_tag_id == None:
+    raise Exception('At least a "container_id" or "container_tag_id" must be specified.')
+  
+  if container_tag_id:
+    if not container_tag_id.startswith('GTM-'):
+      raise Exception("Invalid Container Tag ID. Must be somethink like GTM-XXXXXX")
+    
+    container = lookup_container_by_container_tag_id(container_tag_id=container_tag_id)
+    container_id = container['containerId']
 
   access_list_for_this_account = list_tag_manager_accesses(account_id=account_id)
 
@@ -199,5 +230,5 @@ def add_or_update_permission(
       account_permission=account_permission,
       container_permission=container_permission
     )
-  
+
   return response
